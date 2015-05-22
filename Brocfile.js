ae29d6de5586e75b34951f1d71f7947c4b7a49d0
tkheyfets <to@timur.io>
2015-05-22 18:18:54 -0400

    models, adapters - initial commit

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,15 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  outputPaths: {
+    app: {
+      css: {
+        'app': '/assets/screen.css'
+      }
+    }
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
