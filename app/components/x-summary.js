0063d46886b7ee4ae2451bba48aa5030d146c241
tkheyfets <to@timur.io>
2015-05-25 15:30:54 -0400

    - fixed header styles - added photos total on sidebar

--- app/components/x-summary.js
+++ app/components/x-summary.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['panel'],
+  classNameBindings: ['isAlbum:panel-extended'],
+  isAlbum : function () {
+    return !!this.get('album.name');
+  }.property('album.name'),
   actions : {
     remove: function (album) {
       if (confirm('Are you certain that you want to remove %@?'.fmt(album.get('name')))) {
